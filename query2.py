d92576e96d0db9a3411fa247d6456b71571fef31
emerya2002 <95879915+emerya2002@users.noreply.github.com>
2024-04-19 02:25:29 -0400

    query2 done

--- query2.py
+++ query2.py
@@ -2,29 +2,71 @@ import re
 from pymongo import MongoClient
 import matplotlib.pyplot as plt
 
-def is_english(s):
-    words = ["the", "be", "to", "of", "and", "a", "in", "that", "have", "I", "it", "for", "not", "on", "with", "he", "as", "you", "do", "at", "this", "but", "his", "by", "from", "they", "we", "say", "her", "she", "or", "an", "will", "my", "one", "all", "would", "there", "their", "what", "so", "up", "out", "if", "about", "who", "get", "which", "go", "me", "when", "make", "can", "like", "time", "no", "just", "him", "know", "take", "people", "into", "year", "your", "good", "some", "could", "them", "see", "other", "than", "then", "now", "look", "only", "come", "its", "over", "think", "also", "back", "after", "use", "two", "how", "our", "work", "first", "well", "way", "even", "new", "want", "because", "any", "these", "give", "day", "most", "us"]
-    for word in s.split():
-        if word.lower() not in words:
-            return False
-    return True
-
 client = MongoClient('mongodb://localhost:27017/')
 result = client['Spotify_Youtube']['songs'].aggregate([
     {
         "$match": {"Description": {"$exists": True, "$ne": ""}}
+    },
+    {
+        "$addFields": {
+            "English_Word_Count": {
+                "$size": {
+                    "$filter": {
+                        "input": {
+                            "$regexFindAll": {
+                                "input": {"$toLower": "$Description"},
+                                "regex": r"\b(the|is|are|in|on|at|and|or|of|to|a|an|it|this|that|with|for|from|by|as|but|not|have|has|had|do|does|did|you|we|they|he|she|it|i|my|your|our|his|her|its|their|one|two|three|four|five|six|seven|eight|nine|ten)\b"
+                            }
+                        },
+                        "as": "match",
+                        "cond": {"$ne": ["$$match", []]}
+                    }
+                }
+            }
+        }
+    },
+    {
+        "$addFields": {
+            "Language": {
+                "$cond": {"if": {"$gte": ["$English_Word_Count", 3]}, "then": "English", "else": "Non-English"}
+            }
+        }
+    },
+    {
+        "$group": {
+            "_id": "$Language",
+            "avg_streams": {"$avg": "$Stream"},  # Changed to $avg to calculate average
+            "avg_views": {"$avg": "$Views"},
+            "avg_likes": {"$avg": "$Likes"},
+            "avg_comments": {"$avg": "$Comments"}
+        }
     }
 ])
 
-english_views = 0
-non_english_views = 0
+# Store the averages in dictionaries for plotting
+english_metrics = {}
+non_english_metrics = {}
 
 for doc in result:
-    print(doc)
+    if doc["_id"] == "English":
+        english_metrics = doc
+    elif doc["_id"] == "Non-English":
+        non_english_metrics = doc
 
-""" Plotting
-plt.bar(["TODO"])
-plt.xlabel("TODO")
-plt.ylabel("TODO")
-plt.title("TODO")
-plt.show() """
+# Plotting the results
+labels = ['Streams', 'Views', 'Likes', 'Comments']
+english_values = [english_metrics["avg_streams"], english_metrics["avg_views"], english_metrics["avg_likes"], english_metrics["avg_comments"]]
+non_english_values = [non_english_metrics["avg_streams"], non_english_metrics["avg_views"], non_english_metrics["avg_likes"], non_english_metrics["avg_comments"]]
+
+x = range(len(labels))
+
+plt.bar(x, english_values, label='English')
+plt.bar([i + 0.4 for i in x], non_english_values, label='Non-English')
+
+plt.xlabel('Metrics')
+plt.ylabel('Average')
+plt.title('Average Metrics by Language')
+plt.xticks([i + 0.2 for i in x], labels)
+plt.legend()
+
+plt.show()
